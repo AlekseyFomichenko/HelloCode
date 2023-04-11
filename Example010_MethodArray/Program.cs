d801facb08b9592f551646255b8d08fe1d4d2a38
Aleksey <fomichaaf.95@gmail.com>
2023-04-11 09:51:29 +0300

    Итоги второй лекции

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,14 @@
+﻿int[] array = { 2, 235, 2, 256, 8, 4572, 632335, 88413, 356, 4, 46, 15 };
+int n = array.Length;
+int find = 4;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;   
+    }
+    index++;
+    if (index == n) Console.WriteLine("not found");
+}
