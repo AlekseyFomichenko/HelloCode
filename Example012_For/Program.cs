f8c47b56aecd68f52661be0c25d925a9d16b4ce8
Aleksey <fomichaaf.95@gmail.com>
2023-04-16 21:32:46 +0300

    lesson3_For

---
+++ Example012_For/Program.cs
@@ -0,0 +1,20 @@
+﻿void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+int[] myArray = { 1, 5, 7, 3, 75, 90, 6, 3, 4, 1 };
+SelectionSort(myArray);
+for (int i = 0; i < myArray.Length - 1; i++)
+{
+    Console.Write(myArray[i]);
+}
