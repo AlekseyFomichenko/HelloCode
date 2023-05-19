4916c36ec8be92d4d6444aabe6f6f57950cf30fe
Aleksey <fomichaaf.95@gmail.com>
2023-05-19 23:11:55 +0300

    got acquainted with the topic of Recursion

---
+++ Example013_Recursion/Program.cs
@@ -0,0 +1,119 @@
+﻿// Собрать строку м числами от а до б
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for (int i = a; i <= b; i++)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+string NumbersRec(int a, int b)
+{
+    if (a <= b) return $"{a} " + NumbersRec(a + 1, b);
+    else return string.Empty;
+}
+
+// Сумма чисел от 1 до n
+int Sum(int n)
+{
+    int result = 0;
+    for (int i = 0; i <= n; i++) result += i;
+    return result;
+}
+int SumRec(int n)
+{
+    if (n == 0) return 0;
+    else return n + SumRec(n - 1);
+}
+
+// найти факторил числа n
+int Factorial(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++) result *= i;
+    return result;
+}
+int FactorialRec(int n)
+{
+    if (n == 0) return 1;
+    else return n * FactorialRec(n - 1);
+}
+
+// вычислить а^n
+int PowerFor(int a, int n)
+{
+    int result = 1;
+    for (int i = 0; i <= n; i++) result *= a;
+    return result;
+}
+int PowerRec(int a, int n)
+{
+    return n == 0 ? 1 : PowerRec(a, n - 1) * a;
+}
+int PowerRecMath(int a, int n)
+{
+    if (n == 0) return 1;
+    else if (n % 2 == 0) return PowerRecMath(a * a, n / 2);
+    else return PowerRecMath(a, n - 2) * a;
+}
+
+// перебор слов
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        Console.WriteLine($"{n++} {new String(word)}");
+        return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[4]);
+
+// вывести на консоль каталог
+string path = @"C:\Users\Aleksey\Documents\VS Code\Знакомство с языком программирования C#(лекции)\Examples";
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+CatalogInfo(path);
+
+// игра в пирамидки
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count - 1) Towers(some, on, with, count - 1);
+}
+Towers();
+
+// обход разных структур
+string emp = String.Empty;
+string[] tree = { emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3" };
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        if (left < tree.Length && !String.IsNullOrEmpty(tree[left])) InOrderTraversal(left);
+        Console.WriteLine(tree[pos]);
+        if (right < tree.Length && !String.IsNullOrEmpty(tree[right])) InOrderTraversal(right);
+    }
+}
